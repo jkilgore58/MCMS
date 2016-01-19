e709a6bc8ea6b565bf119c33c159934a3f34ccab
Jonathan Kilgore <jonathan.kilgore58@gmail.com>
2016-01-19 15:22:51 -0600

    updated Add button ccode

--- MCMS/ViewController.m
+++ MCMS/ViewController.m
@@ -11,8 +11,9 @@
 
 @interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (strong, nonatomic) IBOutlet UITextField *addNameTextField;
-@property (strong, nonatomic) IBOutlet NSLayoutConstraint *addWeaponTextField;
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UITextField *descriptionTextField;
+
 @property NSMutableArray *creatures;
 
 @end
@@ -43,10 +44,24 @@
 
 - (IBAction)onBattleButtonPressed:(UIBarButtonItem *)sender {
 }
-- (IBAction)onAddButtonPressed:(UIButton *)sender {
 
-    NSString *addToList = self.addNameTextField.text;
-    [self.creatures addObject:addToList];
+
+- (IBAction)onAddButtonPressed:(UIButton *)sender {
+    
+    NSString *newName = self.addNameTextField.text;
+    NSString *newDescription = self.descriptionTextField.text;
+    UIImage *newImage = [UIImage imageNamed:@"newCreature"];
+    NSMutableArray *newAccessory = [NSMutableArray arrayWithObjects:@"Longbow", @"Hammer", @"Sword", nil];
+    
+    MagicalCreature *newCreature = [[MagicalCreature alloc]initWithName:newName
+                                                             andDetails:newDescription
+                                                           andAccessory:nil
+                                                       andCreatureImage:nil];
+    
+    
+    [self.creatures addObject:newCreature];
+    
+    
     self.addNameTextField.text = nil;
     [self.tableView reloadData];
 }
